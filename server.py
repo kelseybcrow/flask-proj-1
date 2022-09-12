297aef163e24c362de6d3f5ae30ef61e1fb1aafd
kelseybcrow <kelseybcrow@gmail.com>
2022-09-12 08:32:33 -0600

    create folders for templates and static

--- server.py
+++ server.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/endpoint')
-
 @app.route('/')
 def home():
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/cupcakes')
+def all_cupcakes():
+    return render_template('allCupcakes.html')
+
+@app.route('/cupcake_individual')
+def cupcake_individual():
+    return render_template('cupcake.html')
+
+@app.route('/order')
+def order():
+    return render_template('activeOrder.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.env = 'development'
+    app.run(debug = True, port = 8000, host = 'localhost')
