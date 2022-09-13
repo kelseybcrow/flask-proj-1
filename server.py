481be11b41ecd1bb59f10c002d7b956c96c4ec5f
kelseybcrow <kelseybcrow@gmail.com>
2022-09-12 18:00:50 -0600

    build out server endpoints more

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
+
+from cupcakes import get_cupcakes, find_cupcake, add_cupcake_dictionary
 
 @app.route('/')
 def home():
@@ -10,23 +12,37 @@ def home():
 def all_cupcakes():
     return render_template('allCupcakes.html')
 
-@app.route('/cupcake_individual')
-def cupcake_individual():
-    return render_template('cupcake.html')
-
-@app.route('/order')
-def order():
-    return render_template('activeOrder.html')
-
 @app.route('/add-cupcake/<name>')
 def add_cupcake(name):
     cupcake = find_cupcake('cupcakes.csv', name)
 
         if cupcake:
-            add_cupcake_dictionary('orders.csv', cupcake)
+            add_cupcake_dictionary('orders.csv', cupcake=cupcake)
             return redirect(url_for('home'))
         else:
             return 'Sorry, cupcake not found.'
+
+@app.route('/individual-cupcake/<name>')
+def individual_cupcake(name):
+    cupcake = find_cupcake('cupcakes.csv', name)
+
+    if cupcake:
+        return render_template('individual-cupcake.html', cupcake=cupcake)
+
+    else:
+        return 'Sorry, cupcake not found.'    
+
+@app.route('/order')
+def order():
+    cupcakes=get_cupcakes('orders.csv')
+
+    cupcakes_counted = []
+    cupcake_set = set()
+
+    for cupcake in cupcakes:
+        cupcake_set.add((cupcake['name'], cupcake['price'], cupcakes.count(cupcake)))
+
+    return render_template('order.html', cupcakes=cupcake_set)
 
 if __name__ == '__main__':
     app.env = 'development'
